e96e88169811e1db8d0a64d6bd3ca77b4f525914
CaptMac San® <cpt.macp@gmail.com>
2019-05-12 16:35:40 +0530

    derp
    >fixed some changes
    >using pixel color :- #ff1a73e8
    >fixed indentation

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 function TodoCtrl($scope) {
-  
+
   $scope.todos = [
-    {text:'Learn AngularJS', done:false},         
-    {text: 'Build an app', done:false}
+    { text: 'List Item 1', done: true },
+    { text: 'List Item 2', done: false }
   ];
-  
+
   $scope.getTotalTodos = function () {
     return $scope.todos.length;
   };
-  
-  
+
+
   $scope.addTodo = function () {
-    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.todos.push({ text: $scope.formTodoText, done: false });
     $scope.formTodoText = '';
   };
-  
-    $scope.clearCompleted = function () {
-        $scope.todos = _.filter($scope.todos, function(todo){
-            return !todo.done;
-        });
-    };
+
+  $scope.clearCompleted = function () {
+    $scope.todos = _.filter($scope.todos, function (todo) {
+      return !todo.done;
+    });
+  };
 }
